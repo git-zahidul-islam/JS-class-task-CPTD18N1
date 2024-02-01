de292eaff7c9d0c29d764e0ea641eaa46a726ffd
git-zahidul-islam <freelancerzahidf101@gmail.com>
2024-02-01 16:12:56 +0600

    subtask is complete

--- forTask3.js
+++ forTask3.js
@@ -8,5 +8,17 @@ for (i = 91; i <= 129; i++) {
         sum += currentNumber;
     }
 }
-console.log(sum);
+// console.log(sum);
 
+// Display sum of all the even numbers from 51 to 85.
+
+let sum2 = 0;
+for (i = 51; i <= 85; i++) {
+    if (i % 2 === 0) {
+        // console.log(i)
+        const currentNumber2 = i;
+        sum2 += currentNumber2;
+    }
+}
+console.log(sum2);
+// subtask 2 is complete
