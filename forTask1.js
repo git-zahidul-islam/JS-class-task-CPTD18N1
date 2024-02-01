d0d0a963d05b0a50650f53ac74f9e3c5dae37b90
git-zahidul-islam <freelancerzahidf101@gmail.com>
2024-02-01 15:19:22 +0600

    task 1 is complete

--- forTask1.js
+++ forTask1.js
@@ -1,1 +1,7 @@
-console.log("hello")
+// task 01
+const massages = "I will invest at least 6 hrs every single day for next 60 days!";
+
+for (i = 1; i <= 60; i++) {
+    console.log(i, massages);
+}
+// task is complete 
