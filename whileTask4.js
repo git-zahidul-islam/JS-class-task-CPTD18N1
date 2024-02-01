7ac5268b0213f42c449a49abc1c9e847b145993e
git-zahidul-islam <freelancerzahidf101@gmail.com>
2024-02-01 18:50:12 +0600

    for and while is complete

---
+++ whileTask4.js
@@ -0,0 +1,10 @@
+// As Ersa is learning now, she wants to explore more and more. Tell Ersa to generate a multiplication table for number 5
+let multi = 1;
+let i = 1;
+while (i <= 5) {
+    const currentNumber = i;
+    multi = multi * currentNumber;
+    i++;
+}
+console.log(multi);
+// the task is complete
