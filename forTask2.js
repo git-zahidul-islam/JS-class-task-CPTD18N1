b892eb76105a3c0a835a96ddac20cea9c4d6e540
git-zahidul-islam <freelancerzahidf101@gmail.com>
2024-02-01 15:40:04 +0600

    tast 2 is complete

---
+++ forTask2.js
@@ -0,0 +1,15 @@
+// task 2
+// the odd numbers from 61 to 100.
+for (i = 60; i <= 100; i++) {
+    if (i % 2 === 0) {
+        // console.log(`Even number:`, i)
+    }
+};
+
+// the even numbers from 78 to 98.
+for (i = 78; i <= 98; i++) {
+    if (i % 2 !== 0) {
+        console.log(`Odd number:`, i)
+    }
+};
+
