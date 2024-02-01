1b3b0ec056bfc7475bb76cb85c25dc4a7640ebb8
git-zahidul-islam <freelancerzahidf101@gmail.com>
2024-02-01 16:28:30 +0600

    task 4 is complete

---
+++ forTask4.js
@@ -0,0 +1,12 @@
+// Generate a multiplication table for number 9
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+let multi = 9;
+for (i = 1; i <= numbers.length; i++) {
+    const result = i;
+    multi = multi * i;
+}
+console.log(multi);
+
+// task 4 is complete 
